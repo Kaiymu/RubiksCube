c53e5aef7f27ad18619c134c7a2931df30fa246d
Tfontaine <tom_1993@hotmail.fr>
2020-02-02 14:35:10 +0100

    [CODE] - Adding dynamic cube creation (CubeManager) - Rotation using A Z from keyboard - Raycast script to raycast drag our cube - Empty GameManager to handle our futur game states.

---
+++ Assets/Scripts/Cube/RaycastCubeDrag.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastCubeDrag : MonoBehaviour
+{
+    private MiniCube _miniCubeSelected;
+    private Vector3 _previousMousePos;
+
+    // TODO : Refactor with Input.Touch, for now we'll use editor for our test
+    private void Update() {
+        _RaycastMiniCube();
+        _UnselectCube();
+        _DirectionDrag();
+    }
+
+    private void _RaycastMiniCube() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            int miniCubeLayer = LayerMask.GetMask("MiniCube");
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, miniCubeLayer)) {
+
+                Debug.Log("Touched mini cube");
+                _miniCubeSelected = hit.transform.GetComponent<MiniCube>();
+                _miniCubeSelected.selected = true;
+            }
+        }
+    }
+
+    private void _UnselectCube() {
+        if (Input.GetMouseButtonUp(0)) {
+            _miniCubeSelected = null;
+        }
+    }
+
+    private void _DirectionDrag() {
+        if (Input.GetMouseButton(0) && _miniCubeSelected != null) {
+            if (_previousMousePos.x < Input.mousePosition.x) {
+                Debug.LogError(_miniCubeSelected.xyz.y);
+                CubeManager.Instance._GetSameHorizontalCubes(_miniCubeSelected.xyz.y);
+                Debug.LogError("Go right"); // Make to rotation happen here
+            }
+            else if (_previousMousePos.x > Input.mousePosition.x) {
+                Debug.LogError("Go left");
+            }
+
+            if (_previousMousePos.y < Input.mousePosition.y) {
+                Debug.LogError("Go top");
+            }
+            else if (_previousMousePos.y > Input.mousePosition.y) {
+                Debug.LogError("Go bottom");
+            }
+
+            _previousMousePos = Input.mousePosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/CubeManager.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public MiniCube miniCube;
+    public int numberCubes = 2;
+
+    // TODO : Remove, just for test purpose before getting the column / row of the cube
+    public int ySize;
+    public int xSize;
+
+    private List<MiniCube> _miniCubes = new List<MiniCube>();
+
+    private Dictionary<string, Transform> _parentTransform = new Dictionary<string, Transform>();
+
+    public static CubeManager Instance;
+
+    private void Awake() {
+        if (Instance == null)
+            Instance = this;
+
+        _CreateCubes();
+        _CreateParentRotation();
+    }
+
+    ///<summary>
+    /// Create our cubes and add them inside our list.
+    /// Each cubes will have a set of basic position, we assume they are 1x1x1 on scale.
+    ///</summary>
+    private void _CreateCubes() {
+        // Because of floating point imprecision i'd rather use int that floats
+        Vector3Int cubePosition = Vector3Int.zero;
+
+        for (int i = 0; i < numberCubes; i++) {
+            for (int j = 0; j < numberCubes; j++) {
+                for (int k = 0; k < numberCubes; k++) {
+                    // X, Y, Z
+                    cubePosition = new Vector3Int(k, j, i);
+                    MiniCube newMiniCube = Instantiate(miniCube, cubePosition, Quaternion.identity);
+                    newMiniCube.xyz = cubePosition;
+                    newMiniCube.transform.position = cubePosition;
+                    _miniCubes.Add(newMiniCube);
+                }
+            }
+        }
+    }
+    private void Update() {
+        // Just to test basic rotation, the drag code to touch our cubes will replace that
+        if (Input.GetKeyDown(KeyCode.A)) {
+            _GetSameHorizontalCubes(ySize);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            _GetSameVerticalCubes(xSize);
+        }
+    }
+
+    // TODO : Refactor to get one function for horizontal vertical
+    public void _GetSameHorizontalCubes(int y) {
+        var miniCubeHorizontal = new List<MiniCube>();
+
+        for (int i = 0; i < _miniCubes.Count; i++) {
+            // We round them to int because of floating precision issue.
+            int miniCubeYPos = Mathf.RoundToInt(_miniCubes[i].transform.position.y);
+            if (miniCubeYPos == y) {
+                Debug.LogError("Row " + miniCubeYPos);
+                miniCube.xyz = new Vector3Int(miniCube.xyz.x, miniCubeYPos, miniCube.xyz.z);
+                miniCubeHorizontal.Add(_miniCubes[i]);
+
+            }
+        }
+
+        _RotateParentCube(miniCubeHorizontal, false);
+    }
+
+    // TODO : Refactor to get one function for horizontal vertical
+    public List<MiniCube> _GetSameVerticalCubes(int x) {
+        var miniCubeVertical = new List<MiniCube>();
+
+        for (int i = 0; i < _miniCubes.Count; i++) {
+            var miniCube = _miniCubes[i];
+            // We round them to int because of floating precision issue.
+            int miniCubeXPos = Mathf.RoundToInt(miniCube.transform.position.x);
+            
+            // Getting all the cubes from the same row
+            if (miniCubeXPos == x) {
+                Debug.LogError("Column " + miniCubeXPos);
+                miniCube.xyz = new Vector3Int(miniCubeXPos, miniCube.xyz.y, miniCube.xyz.z);
+                miniCubeVertical.Add(_miniCubes[i]);
+            }
+        }
+
+        _RotateParentCube(miniCubeVertical, true);
+
+        return miniCubeVertical;
+    }
+
+
+    private void _RotateParentCube(List<MiniCube> miniCubeRotation, bool vertical) {
+
+        Transform parentTransform = null;
+
+        string parentName = vertical ? "row" + ySize : "column" + xSize;
+
+        // Getting our parent from our dictionnary
+        if (_parentTransform.TryGetValue(parentName, out parentTransform)) {
+
+            // Settings our parents for all the childs that we got
+            // TODO : Refactor to get that throught LINQ.
+            for (int i = 0; i < miniCubeRotation.Count; i++) {
+                miniCubeRotation[i].transform.parent = parentTransform.transform;
+            }
+
+            // We only go one way, but when we'll have our drag direction
+            if (vertical)
+                parentTransform.transform.Rotate(new Vector3(90, 0, 0));
+            else
+                parentTransform.transform.Rotate(new Vector3(0, 90, 0));
+
+            // Unsetting all the parents
+            for (int i = 0; i < miniCubeRotation.Count; i++) {
+                miniCubeRotation[i].transform.parent = null;
+            }
+        }
+        
+    }
+
+    ///<summary>
+    /// Create a parent for each row / columns at the center of each.
+    /// They're used as temporary pivot point for our MiniCubes
+    /// We can access these parents throught a Dictionnary
+    ///</summary>
+    private void _CreateParentRotation() {
+
+        // TODO : Refactor to only do one loop.
+        for (int i = 0; i < numberCubes; i++) {
+            // We get the middle position of our farthest cubes in the current vertical row.
+            Vector3 positionHVerticalParent = Vector3.Lerp(new Vector3(0, i, 0), new Vector3(numberCubes - 1, i, numberCubes - 1), 0.5f);
+
+            // We create our object, set it's position and add it to the dictionnary while giving it a unique name.
+            var parentRotation = new GameObject();
+            parentRotation.transform.localEulerAngles = Vector3.zero;
+            parentRotation.transform.localPosition = positionHVerticalParent;
+            parentRotation.name = "column" + i;
+            _parentTransform.Add(parentRotation.name, parentRotation.transform);
+        }
+
+        for (int i = 0; i < numberCubes; i++) {
+            Vector3 positionHorizontalParent = Vector3.Lerp(new Vector3(i, 0, 0), new Vector3(i, numberCubes - 1, numberCubes - 1), 0.5f);
+
+            var fakeParent = new GameObject();
+            fakeParent.transform.localEulerAngles = Vector3.zero;
+            fakeParent.transform.localPosition = positionHorizontalParent;
+            fakeParent.name = "row" + i;
+            _parentTransform.Add(fakeParent.name, fakeParent.transform);
+        }
+    }
+}

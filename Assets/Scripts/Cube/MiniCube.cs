f33bd9d25f951e6f4e036d3269f84c52e5d7932a
Tfontaine <tom_1993@hotmail.fr>
2020-02-02 14:33:00 +0100

    [PREFAB / CODE] - Adding mini cube prefab with 6 colored faces and basic Cube script to store the cube datas

---
+++ Assets/Scripts/Cube/MiniCube.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniCube : MonoBehaviour
+{
+    public Vector3Int xyz;
+    public bool selected;
+}
